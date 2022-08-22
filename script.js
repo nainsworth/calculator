917ddf95e961fb5e7ed5ce1770ea8da0d6298930
nicholas <nja8161995@gmail.com>
2022-08-22 16:26:31 -0500

    Updated:
    Recreated the math functions

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ function clearCalc() {
   input = "";
 }
 
+function add(num1, num2) {
+  return num1 + num2;
+} 
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
 // function add(input, num) {
 // total = input + num;
 // resultOutput.innerText = total;
