d167f5656e2e7de9af98a87065304796de4fbb8c
nicholas <nja8161995@gmail.com>
2022-08-23 16:32:08 -0500

    Update:
    Clear Button is operational
    
    Current and previous inputs display
    
    Added operator functions
    
    Added Compute function to calculate everything

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const numberBtns = document.querySelectorAll("[data-number]");
-const operatorBtns = document.querySelectorAll("[data-operator]");
-const equalsBtn = document.querySelector("[data-equals]");
-const clearBtn = document.querySelector("[data-clear]");
-const deleteBtn = document.querySelector("[data-delete]");
-const percentBtn = document.querySelector("[data-percent]");
-const inverseBtn = document.querySelector("[data-inverse]");
-const previousTxtElem = document.querySelector("[data-previous]");
-const currentTxtElem = document.querySelector("[data-current]");
-
-// Displays button input onto calculator
-numberBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    appendNumber(button.innerText);
-  });
-});
-
-operatorBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    
-  })
-})
-
 // Onload Creates a clear calculator
 window.onload = function () {
   clear();
@@ -30,14 +7,89 @@ window.onload = function () {
 function clear() {
   currentTxtElem.innerText = 0;
   previousTxtElem.innerText = "";
+  input = "";
+  operation = undefined;
+}
+
+function operate(choice) {
+  if (previousTxtElem.innerText !== "") {
+    compute();
+  }
+  this.choice = choice;
+  previousTxtElem.innerText = currentTxtElem.innerText;
+  currentTxtElem.innerText = 0;
+  input = "";
 }
 
 // Appends button input onto current section of the display
 function appendNumber(num) {
   if (num === "." && currentTxtElem.innerText.includes(".")) return;
   if (currentTxtElem.innerText.length > 8) return;
-  currentTxtElem.innerText += num;
+  input += num;
+  currentTxtElem.innerText = input;
 }
+
+function add(prev, curr) {
+  return prev + curr
+}
+
+function subtract(prev, curr) {
+  return prev - curr
+}
+
+function multiply(prev, curr) {
+  return prev * curr
+}
+
+function divide(prev, curr) {
+  if (curr === "0") {
+    currentTxtElem.innerText = "NOPE";
+    previousTxtElem.innerText = "";
+    input = "";
+  }
+  return prev / curr
+}
+
+function compute() {
+  const prev = parseFloat(previousTxtElem.innerText);
+  const curr = parseFloat(currentTxtElem.innerText);
+
+  if (operation === "+"){}
+}
+
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const equalsBtn = document.querySelector("[data-equals]");
+const clearBtn = document.querySelector("[data-clear]");
+const deleteBtn = document.querySelector("[data-delete]");
+const percentBtn = document.querySelector("[data-percent]");
+const inverseBtn = document.querySelector("[data-inverse]");
+const previousTxtElem = document.querySelector("[data-previous]");
+const currentTxtElem = document.querySelector("[data-current]");
+let input = "";
+let operation = undefined;
+
+// Displays button input onto calculator
+numberBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    appendNumber(button.innerText);
+  });
+});
+
+// Event listener to listen for operator choice
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    operate(button.innerText);
+  });
+});
+
+equalsBtn.addEventListener("click", (button) => {
+  compute();
+});
+
+clearBtn.addEventListener("click", (button) => {
+  clear();
+});
 
 // // let input = 0;
 // // let total = 0;
