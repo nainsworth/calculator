2618fec84b745e6f6e57a06ea46ef9e90655c57f
nicholas <nja8161995@gmail.com>
2022-08-23 15:08:04 -0500

    Update:
    Restyled display digits to fit properly
    
    Complete recoded the js file into a better layout
    
    Added a feature where "." can only be used once.
    
    Created a feature where the calculator cannot displays more than 9 digits

--- script.js
+++ script.js
@@ -1,57 +1,95 @@
-const previousOutput = document.getElementById("previous-output");
-const currentOutput = document.getElementById("current-output");
-// let input = 0;
-// let total = 0;
-let blankCalc = 0
-let input = "";
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const equalsBtn = document.querySelector("[data-equals]");
+const clearBtn = document.querySelector("[data-clear]");
+const deleteBtn = document.querySelector("[data-delete]");
+const percentBtn = document.querySelector("[data-percent]");
+const inverseBtn = document.querySelector("[data-inverse]");
+const previousTxtElem = document.querySelector("[data-previous]");
+const currentTxtElem = document.querySelector("[data-current]");
 
+// Displays button input onto calculator
+numberBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    appendNumber(button.innerText);
+  });
+});
 
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    
+  })
+})
+
+// Onload Creates a clear calculator
 window.onload = function () {
-  currentOutput.innerText = 0;
+  clear();
 };
 
-function displaySelectedNumber(num) {
-   input += num;
-   currentOutput.innerText = input;
+// Clears calculator
+function clear() {
+  currentTxtElem.innerText = 0;
+  previousTxtElem.innerText = "";
 }
 
-function clearCalc() {
-  currentOutput.innerText = 0;
-  input = "";
+// Appends button input onto current section of the display
+function appendNumber(num) {
+  if (num === "." && currentTxtElem.innerText.includes(".")) return;
+  if (currentTxtElem.innerText.length > 8) return;
+  currentTxtElem.innerText += num;
 }
 
-function add(num1, num2) {
-  return num1 + num2;
-} 
+// // let input = 0;
+// // let total = 0;
+// let blankCalc = 0
+// let input = "";
 
-function subtract(num1, num2) {
-  return num1 - num2;
-}
+// window.onload = function () {
+//   currentOutput.innerText = 0;
+// };
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-// function add(input, num) {
-// total = input + num;
-// resultOutput.innerText = total;
+// function displaySelectedNumber(num) {
+//    input += num;
+//    currentOutput.innerText = input;
 // }
 
-// function subtract(input, num) {
-// total = input - num;
-// resultOutput.innerText = total;
+// function clearCalc() {
+//   currentOutput.innerText = 0;
+//   input = "";
 // }
 
-// function multiply(input, num) {
-// total = input * num;
-// resultOutput.innerText = total;
+// function add(num1, num2) {
+//   return num1 + num2;
 // }
 
-// function divide(input, num) {
-// total = input / num;
-// resultOutput.innerText = total;
+// function subtract(num1, num2) {
+//   return num1 - num2;
 // }
+
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// function divide(num1, num2) {
+//   return num1 / num2;
+// }
+
+// // function add(input, num) {
+// // total = input + num;
+// // resultOutput.innerText = total;
+// // }
+
+// // function subtract(input, num) {
+// // total = input - num;
+// // resultOutput.innerText = total;
+// // }
+
+// // function multiply(input, num) {
+// // total = input * num;
+// // resultOutput.innerText = total;
+// // }
+
+// // function divide(input, num) {
+// // total = input / num;
+// // resultOutput.innerText = total;
+// // }
